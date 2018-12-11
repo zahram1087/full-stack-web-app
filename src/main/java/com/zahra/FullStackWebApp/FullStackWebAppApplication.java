ca5b6a7b20a1f3a4450e2a2e4dc76c2779f7f2b7
Zahra <zahram1087@gmail.com>
2018-12-10 22:23:23 -0800

    initial commit after spring boot initialzr

---
+++ src/main/java/com/zahra/FullStackWebApp/FullStackWebAppApplication.java
@@ -0,0 +1,12 @@
+package com.zahra.FullStackWebApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullStackWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FullStackWebAppApplication.class, args);
+	}
+}
