1446fc9628850ecfa5b2b33ae22533cfe9a88816
Zahra <zahram1087@gmail.com>
2018-12-11 11:25:35 -0800

    lab11

---
+++ notes.md
@@ -0,0 +1,24 @@
+
+
+
+
+//    @RequestMapping("/capitalize")
+//    public String capitalizeWords(@RequestParam (name = "name",required =false)String name){
+//        return name.toUpperCase();
+//        //go to: http://localhost:8080/capitalize?name=me
+//
+//    }
+
+//    @RequestMapping("/capitalize/{wordsToCapitalize}")
+//    public String capitalizeWords(@PathVariable()String wordsToCapitalize){
+//        return  wordsToCapitalize.toUpperCase();
+//    }
+
+//    @GetMapping("/capitalize/{wordsToCapitalize}")
+//    public String capitalizeWords(@PathVariable()String wordsToCapitalize, Model model){
+//
+//       model.addAttribute("wordsToCapitalize",wordsToCapitalize);
+//
+//       return wordsToCapitalize.toUpperCase();
+//        //leaf passes the string all the way to the front end
+//    }
--- src/main/java/com/zahra/FullStackWebApp/HelloWorldController.java
+++ src/main/java/com/zahra/FullStackWebApp/HelloWorldController.java
@@ -1,12 +1,28 @@
 package com.zahra.FullStackWebApp;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 
 @RestController //a class that holds routes
-public class HelloWorldController {
-    @RequestMapping("/hello") //get method is default
+public class HelloWorldController  {
+
+    @RequestMapping(value = "/hello", method = RequestMethod.GET) //we are explicitly saying its a get, we can change to post
     public String helloworld(){
         return "Hello world";
+    }
+
+    @RequestMapping(value = "/capitalize/{wordsToCapitalize}", method = RequestMethod.GET)
+    public String capitalizeWords(@PathVariable()String wordsToCapitalize, Model model){
+
+        model.addAttribute("wordsToCapitalize",wordsToCapitalize);
+        return wordsToCapitalize(wordsToCapitalize);
+        //leaf passes the string all the way to the front end
+    }
+
+    public static String  wordsToCapitalize(String wordsToCapitalize){
+
+        return wordsToCapitalize.toUpperCase();
+
     }
 }
